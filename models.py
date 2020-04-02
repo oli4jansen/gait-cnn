3bce4856cb5fc44e73bbcc1803a4996111e03765
Olivier Jansen <oli4jansen.nl@gmail.com>
2020-04-02 15:33:11 +0200

    typo

--- models.py
+++ models.py
@@ -14,7 +14,7 @@ class GaitNet(torch.nn.Module):
         # Replace FC with our layer and enable gradients
         # self.r2plus1d_18.fc = torch.nn.Linear(in_features=512, out_features=512)
 
-        self.r2plus1d.fc = torch.nn.Sequential(
+        self.r2plus1d_18.fc = torch.nn.Sequential(
             torch.nn.Linear(in_features=self.r2plus1d_18.fc.in_features, out_features=512),
             torch.nn.ReLU(),
             torch.nn.Linear(in_features=512, out_features=num_classes)
