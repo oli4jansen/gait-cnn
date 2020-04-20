a6efd951768e9ce28f881740f4e4ecf349f8e84f
Olivier Jansen <oli4jansen.nl@gmail.com>
2020-04-20 10:23:19 +0200

    linear + conv poses

--- models.py
+++ models.py
@@ -21,13 +21,35 @@ class GaitNet(torch.nn.Module):
         self.pose_model = hg2(pretrained=True)
         self.pose_predictor = HumanPosePredictor(self.pose_model)
 
+        self.pose_linear = torch.nn.Sequential(
+            torch.nn.Flatten(start_dim=1),
+            torch.nn.Linear(in_features=512, out_features=128),
+            torch.nn.ReLU()
+        )
+
         self.pose_cnn = torch.nn.Sequential(
-            torchvision.models.video.resnet.Conv2Plus1D(1, 64, 32, padding=2),
-            torchvision.models.video.resnet.Conv2Plus1D(64, 64, 64),
-            torchvision.models.video.resnet.Conv2Plus1D(64, 4, 16),
-            torch.nn.AdaptiveAvgPool3d((3, 3, 1)),
-            torch.nn.Flatten(start_dim=1)
-            # torch.nn.Linear(in_features=36, out_features=num_classes),
+            torch.nn.Conv3d(1, 32, kernel_size=(1, 3, 3),
+                            padding=(0, 0, 1), bias=False),
+
+            torch.nn.Conv3d(32, 64, kernel_size=(1, 3, 3),
+                            padding=(0, 0, 1), bias=False),
+
+            torch.nn.Conv3d(64, 32, kernel_size=(1, 3, 3),
+                            padding=(0, 0, 1), bias=False),
+
+            torch.nn.BatchNorm3d(32),
+            torch.nn.ReLU(inplace=True),
+
+            torch.nn.Conv3d(32, 16, kernel_size=(4, 1, 1),
+                            padding=(1, 0, 0), bias=False),
+
+            torch.nn.Conv3d(16, 16, kernel_size=(5, 1, 1), bias=False),
+
+            torch.nn.BatchNorm3d(16),
+            torch.nn.ReLU(inplace=True),
+
+            torch.nn.Flatten(start_dim=1),
+            torch.nn.Linear(in_features=3520, out_features=128),
         )
 
         self.r2plus1d_18 = r2plus1d_18(pretrained=True)
@@ -43,34 +65,37 @@ class GaitNet(torch.nn.Module):
             param.requires_grad = False
 
         self.classifier = torch.nn.Sequential(
-            torch.nn.Linear(in_features=512 + 36, out_features=num_classes)
+            torch.nn.Linear(in_features=512 + 128 + 128, out_features=128),
+            torch.nn.ReLU(),
+            torch.nn.Linear(in_features=128, out_features=num_classes)
         )
 
     def forward(self, input):
         batch_size, channels, frames, height, width = input.size()
         assert(channels == CHANNELS and frames == FRAMES and height == HEIGHT and width == WIDTH)
 
-        # # Swap channels and frames and upsize to 224x224 for stacked hourglass pose estimator
-        # joints_input = input.permute(0, 2, 1, 3, 4)
-        # # TODO: check torch.nn.Upsample
-        # joints_input = torch.nn.functional.interpolate(joints_input, size=[channels, 224, 224])
-        # # Estimate joints for each sample in batch (pose estimator is implemented for images so video is already batch)
-        # pose_list = [torch.unsqueeze(self.pose_predictor.estimate_joints(i, flip=True), 0) for i in joints_input]
-        #
-        # # Concat tensors in pose list into tensor again
-        # pose_cnn_input = torch.cat(pose_list, dim=0)
-        # # Add an empty channels dimension
-        # pose_cnn_input = torch.unsqueeze(pose_cnn_input, 1)
+        # Swap channels and frames and upsize to 224x224 for stacked hourglass pose estimator
+        joints_input = input.permute(0, 2, 1, 3, 4)
+        # TODO: check torch.nn.Upsample
+        joints_input = torch.nn.functional.interpolate(joints_input, size=[channels, 224, 224])
+        # Estimate joints for each sample in batch (pose estimator is implemented for images so video is already batch)
+        pose_list = [torch.unsqueeze(self.pose_predictor.estimate_joints(i, flip=True), 0) for i in joints_input]
 
-        pose_cnn_input = torch.rand(size=(batch_size, 1, frames, 16, 2))
+        # Concat tensors in pose list into tensor again
+        pose_cnn_input = torch.cat(pose_list, dim=0)
+        # Add an empty channels dimension
+        pose_cnn_input = torch.unsqueeze(pose_cnn_input, 1)
+        #
+        # pose_cnn_input = torch.rand(size=(batch_size, 1, frames, 16, 2))
 
         # Run pose CNN on extracted poses
+        pose_linear_output = self.pose_linear(pose_cnn_input)
         pose_cnn_output = self.pose_cnn(pose_cnn_input)
 
         # Run R(2+1)D on the raw pixel data
         cnn_output = self.r2plus1d_18(input)
 
         # Combine R(2+1)D and pose information for classifier
-        classifier_input = torch.cat([cnn_output, pose_cnn_output], dim=1)
+        classifier_input = torch.cat([cnn_output, pose_linear_output, pose_cnn_output], dim=1)
 
         return self.classifier(classifier_input)
